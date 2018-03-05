11d4de3effcffa69ac6c0004a14d750813e597aa
Bastian Widmer <b.widmer@dasrecht.net>
2018-03-05 13:12:20 +0100

    cleanup and updates

--- lagoon/stagefile/stagefile.sh
+++ lagoon/stagefile/stagefile.sh
@@ -2,8 +2,5 @@
 
 # If LAGOON_ENVIRONMENT_TYPE is set to development we add the stagefile rules
 if [ "$LAGOON_ENVIRONMENT_TYPE" == "development" ]; then
-echo oi!
     cp /etc/nginx/conf.d-available/stagefile-*.conf /etc/nginx/conf.d/
 fi
-
-echo uih!
