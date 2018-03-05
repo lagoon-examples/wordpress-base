ae02d763abd41ccaf71a8c3f68e1fa9e629b170c
Bastian Widmer <b.widmer@dasrecht.net>
2018-03-05 12:33:55 +0100

    updates

--- lagoon/stagefile/stagefile.sh
+++ lagoon/stagefile/stagefile.sh
@@ -2,5 +2,8 @@
 
 # If LAGOON_ENVIRONMENT_TYPE is set to development we add the stagefile rules
 if [ "$LAGOON_ENVIRONMENT_TYPE" == "development" ]; then
-    cp /etc/nginx/conf.d-available/wordpress/stagefile-*.conf /etc/nginx/conf.d/wordpress/  
+echo oi!
+    cp /etc/nginx/conf.d-available/stagefile-*.conf /etc/nginx/conf.d/
 fi
+
+echo uih!
