438b8cb53532e30915bd1c7052ecf5086c1a2a55
Dominik Schilling <dominikschilling+git@gmail.com>
2018-08-08 10:25:41 +0200

    Fix handling of stagefile rules to not be processed on production.

--- lagoon/stagefile/stagefile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# If LAGOON_ENVIRONMENT_TYPE is set to development we add the stagefile rules
-if [ "$LAGOON_ENVIRONMENT_TYPE" == "development" ]; then
-    cp /etc/nginx/conf.d-available/stagefile-*.conf /etc/nginx/conf.d/
-fi
